c9c464c5eba3c6babf434f9b6500a2ffef696e47
Sk <skisme4u@gmail.com>
2021-05-31 15:03:15 +0530

    Updates

---
+++ precache-manifest.d3c6e56ea032fcf67ac8e27406c7cba6.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb9574df1841e1cfff565c33cd5a8fb1",
+    "url": "/keeper/index.html"
+  },
+  {
+    "revision": "a9658d271299326fe917",
+    "url": "/keeper/static/js/2.84a18c28.chunk.js"
+  },
+  {
+    "revision": "858f894f919f7864b815",
+    "url": "/keeper/static/js/main.7db3abd5.chunk.js"
+  },
+  {
+    "revision": "904356601dc1001bd3a5",
+    "url": "/keeper/static/js/runtime-main.7142975c.js"
+  }
+]);
